919f2e846f771b0d1eb46121a8e5401a0221a99e
MohamedElsayed143 <mohmadelkhadry@gmail.com>
2026-01-02 01:18:16 +0200

    Fix API_BASE_URL variable

--- front-end/src/lib/utils.ts
+++ front-end/src/lib/utils.ts
@@ -4,4 +4,5 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "https://instapay-project-production.up.railway.app";
+export const API_BASE_URL = 
+  process.env.NEXT_PUBLIC_API_URL || "https://instapay-project-production.up.railway.app";
