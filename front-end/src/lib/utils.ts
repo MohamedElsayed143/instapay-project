284c118ce265e9c44f8bf93eafc7f0fc7c11b339
MohamedElsayed143 <mohmadelkhadry@gmail.com>
2026-01-01 19:14:26 +0200

    final utils v1

--- front-end/src/lib/utils.ts
+++ front-end/src/lib/utils.ts
@@ -4,4 +4,5 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-export const API_BASE_URL = "http://instapay.infinityfreeapp.com";
+export const API_BASE_URL = 
+  process.env.NEXT_PUBLIC_API_URL || "http://instapay.infinityfreeapp.com";
