812fc7c9d92b3cd0320df82ab34e2e919c97e36d
MohamedElsayed143 <mohmadelkhadry@gmail.com>
2026-01-01 19:06:34 +0200

    Update utils

--- front-end/src/lib/utils.ts
+++ front-end/src/lib/utils.ts
@@ -5,4 +5,4 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 export const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_URL || "${API_BASE_URL}";
+  process.env.NEXT_PUBLIC_API_URL || "http://instapay.infinityfreeapp.com";
